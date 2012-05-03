d4eb69ec796870115d7178de588d83af6ff6ff3b
Nathan Rugg <nmrugg@gmail.com>
2012-05-03 19:49:53 +0800

    Optimized smallWords regex.

--- to-title-case.js
+++ to-title-case.js
@@ -3,20 +3,20 @@
  * Copyright © 2008–2012 David Gouch. Licensed under the MIT License. 
  */
 
-String.prototype.toTitleCase = function () {
-  var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|vs?\.?|via)$/i;
-
-  return this.replace(/([^\W_]+[^\s-]*) */g, function (match, p1, index, title) {
-    if (index > 0 && index + p1.length !== title.length &&
-      p1.search(smallWords) > -1 && title.charAt(index - 2) !== ":" && 
-      title.charAt(index - 1).search(/[^\s-]/) < 0) {
-      return match.toLowerCase();
-    }
-
-    if (p1.substr(1).search(/[A-Z]|\../) > -1) {
-      return match;
-    }
-
-    return match.charAt(0).toUpperCase() + match.substr(1);
-  });
+String.prototype.toTitleCase = function ()
+{
+    var smallWords = /^(a(?:nd?|s|t)?|b(?:ut|y)|en|for|i(?:f|n)|o(?:f|n|r)|t(?:he|o)|v(?:s?\.?|ia))$/i;
+    
+    return this.replace(/([^\W_]+[^\s-]*) */g, function (match, p1, index, title)
+    {
+        if (index > 0 && index + p1.length !== title.length && p1.search(smallWords) > -1 && title.charAt(index - 2) !== ":" && title.charAt(index - 1).search(/[^\s-]/) < 0) {
+            return match.toLowerCase();
+        }
+    
+        if (p1.substr(1).search(/[A-Z]|\../) > -1) {
+            return match;
+        }
+        
+        return match.charAt(0).toUpperCase() + match.substr(1);
+    });
 };
