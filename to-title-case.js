32adef933351892db84039a63d0f9b5624f27ea4
Nathan Rugg <nmrugg@gmail.com>
2015-08-22 09:56:21 +0800

    Updated to master.

--- to-title-case.js
+++ to-title-case.js
@@ -1,22 +1,23 @@
-﻿/* 
- * To Title Case 2.0.1 – http://individed.com/code/to-title-case/
- * Copyright © 2008–2012 David Gouch. Licensed under the MIT License. 
+/**
+ * Originaly from:
+ * To Title Case http://individed.com/code/to-title-case/
+ * Copyright © 2008–2013 David Gouch. Licensed under the MIT License. 
  */
 
-String.prototype.toTitleCase = function ()
+function to_title_case(str)
 {
-    var smallWords = /^(a(?:nd?|s|t)?|b(?:ut|y)|en|for|i(?:f|n)|o(?:f|n|r)|t(?:he|o)|v(?:s?\.?|ia))$/i;
+    var smallWords = /^(a(?:nd?|s|t)?|b(?:ut|y)|en|(?:f|n)or|i(?:f|n)|o(?:f|n|r)|per|t(?:he|o)|v(?:s?\.?|ia))$/i;
     
-    return this.replace(/([^\W_]+[^\s-]*) */g, function (match, p1, index, title)
+    return str.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title)
     {
-        if (index > 0 && index + p1.length !== title.length && p1.search(smallWords) > -1 && title.charAt(index - 2) !== ":" && title.charAt(index - 1).search(/[^\s-]/) < 0) {
+        if (index > 0 && index + match.length !== title.length && match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" && (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') && title.charAt(index - 1).search(/[^\s-]/) < 0) {
             return match.toLowerCase();
         }
     
-        if (p1.substr(1).search(/[A-Z]|\../) > -1) {
+        if (match.substr(1).search(/[A-Z]|\../) > -1) {
             return match;
         }
-        
+    
         return match.charAt(0).toUpperCase() + match.substr(1);
     });
 };
